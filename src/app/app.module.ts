e9f423ba301afb66701e357fc8747bc35f1f9031
Steve Harrigan <def.monk0@gmail.com>
2017-10-20 22:47:23 -0400

    Initial setup of all navigation, with some basic page content.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+//
+
+import { AppComponent } from "./app.component";
+import { AuthenticationComponent } from "./authentication/authentication.component";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { HomeComponent } from "./home/home.component";
+
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
+	declarations: [
+		AppComponent,
+		AuthenticationComponent,
+		CalendarComponent,
+		HomeComponent,
+	],
+	imports: [AppRoutingModule, BrowserModule],
+	providers: [],
 })
-export class AppModule { }
+export class AppModule {}
